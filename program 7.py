7bb02bff1013b729108f8d286934e874b078f85f
panneerselvam0115 <smilyboyselvam61@gmail.com>
2025-08-11 10:16:23 +0530

    program 7

---
+++ program 7.py
@@ -0,0 +1,7 @@
+print("circle  program")
+radius=int(input("enter the radius value:"))
+print("result")
+perimeter=2*22/7*radius
+print("perimeter of circle:",perimeter)
+area=22/7*radius*radius
+print("area of circle:",area)
