46d257baa37592908c2dc3ba8a9750fe29d25054
panneerselvam0115 <smilyboyselvam61@gmail.com>
2025-08-11 10:15:47 +0530

    program 6

---
+++ program 6.py
@@ -0,0 +1,14 @@
+print("square and rectangle")
+print("______________________")
+a=int(input("enter the area value:"))
+l=int(input("enter the length value:"))
+b=int(input("enter the bredth value:"))
+print("result")
+ps=4*a
+print("perimeter of square:",ps)
+area=a*a
+print("area of square:",area)
+pr=2*(l*b)
+print("perimeter of rectangle:",pr)
+ar=l*b
+print("area of rectangle:",ar)
