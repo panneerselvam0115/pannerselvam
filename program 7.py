97d84bbf5003ac42903d3d29be1e8dbd066ca436
panneerselvam0115 <smilyboyselvam61@gmail.com>
2025-08-11 10:42:42 +0530

    Delete program 7.py

--- program 7.py
+++
@@ -1,7 +0,0 @@
-print("circle  program")
-radius=int(input("enter the radius value:"))
-print("result")
-perimeter=2*22/7*radius
-print("perimeter of circle:",perimeter)
-area=22/7*radius*radius
-print("area of circle:",area)
